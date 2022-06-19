a1662c48d04f1035da005b54424c3362ae753e1d
joaofelix27 <joaofelix27@gmail.com>
2022-06-19 16:11:50 -0300

    Fazendo Validação de Dados de Sign-up e tweet (bônus 01)

--- index.js
+++ index.js
@@ -10,14 +10,25 @@ const tweets = [];
 
 app.post('/sign-up', (req, res) => {
   const userData = req.body; // será o objeto com os dados de login
-  usersData.push(userData);
+  const userAvatar= userData.avatar
+  const userName= userData.username
+  if (userName && userAvatar ) {
+    usersData.push(userData);
   res.status(201).send("OK");
+  } else {res.status(400).send("Todos os campos são obrigatórios!");}
+  
 });
 app.post('/tweets', (req, res) => {
-    const userAvatar= usersData.find(user => user.username===req.body.username)
-    const tweet = {... req.body, avatar:userAvatar.avatar}; // será o objeto com os dados do  tweet
-    tweets.push(tweet);
-    res.status(201).send("OK");
+    const tweetData=req.body
+    const userName=tweetData.username
+    const userTweet=tweetData.tweet
+    if (userName && userTweet ) {
+      const userAvatar= usersData.find(user => user.username===tweetData.username)
+      const tweet = {... tweetData, avatar:userAvatar.avatar}; // será o objeto com os dados do  tweet
+      tweets.push(tweet);
+      res.status(201).send("OK");
+    } else {res.status(400).send("Todos os campos são obrigatórios!");}
+   
   });
 app.get('/tweets', (req, res) => {
     let lastTweets = tweets.slice(-10)
